f92242a0f0a154117a99b5aefec705ca50be56f9
TheFirstGhost <geislu01@thu.de>
2022-01-14 16:12:03 +0100

    deleted output (musst compiled new)

--- output/CMakeFiles/launch-playground-RelWithDebInfo.cmd
+++
@@ -1,9 +0,0 @@
-cd C:/Users/Lukas/CLionProjects/THU/CG/OpenGL-RenderingMultipleObjects/playground/
-set 
-
-if [%1]==[--debugger] (
-	SHIFT
-)
-
-"C:\Users\Lukas\CLionProjects\THU\CG\OpenGL-RenderingMultipleObjects\output\RelWithDebInfo\playground.exe"  
-pause
--- output/CMakeFiles/launch-playground-Release.cmd
+++
@@ -1,9 +0,0 @@
-cd C:/Users/Lukas/CLionProjects/THU/CG/OpenGL-RenderingMultipleObjects/playground/
-set 
-
-if [%1]==[--debugger] (
-	SHIFT
-)
-
-"C:\Users\Lukas\CLionProjects\THU\CG\OpenGL-RenderingMultipleObjects\output\Release\playground.exe"  
-pause
